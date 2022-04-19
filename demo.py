6bdd3ce04a53383515142e13a8d0713d327c0352
tenkola <1398856707@qq.com>
2022-04-19 20:24:48 +0800

    feat: 5个数据类型

--- demo.py
+++ demo.py
@@ -7,3 +7,21 @@ print(cont)  # print是命令函数打印工作台 一般用于日志
 print(cont[0])
 print(cont[1])
 print(cont[-1])
+print(cont[int(len(cont) / 2)])
+
+# 数值
+number = 1
+m = 0.5
+print(number)
+print(m)
+
+# list
+listhydufjs = [1, 2, "m"]
+print(listhydufjs[0])
+# 元组
+
+into = (1, 2, 9)
+print(into[1])
+# 字典
+ep = {'name': 'runoob', 'code': 6734, 'dept': 'sales'}
+print(ep['code'])
