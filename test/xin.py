4e5dc4e17295232cfbb3afbdf7d8cac43b31e737
tenkola <1398856707@qq.com>
2022-04-25 19:45:21 +0800

    feat：作业完成耶耶

--- test/xin.py
+++
@@ -1,8 +0,0 @@
-number = int(input("请输入任意一整数"))
-print("")
-if number == 1:
-    print("1不是素数")
-elif number == 3:
-    print("3是素数")
-else:
-    print("除了一和它自身外，不能被其他数整除的是素数")
--- work/task1.py
+++ work/task1.py
@@ -2,6 +2,12 @@
 python基础
 """
 
+
 # 1. 搭建个人博客 参考: https://onefeng.xyz/2021/07/10/jekyll/
 
 # 2. 定义一个函数判断一个数是否为素数。提示：需要用到for循环,if语句
+def is_number(x):
+    for i in range(2, x - 1):
+        if x % i == 0:
+            return False
+    return True
